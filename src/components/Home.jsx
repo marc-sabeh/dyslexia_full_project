51e1d8e87464652de97948c7122d188d85eb4616
marc-sabeh <sabehmarc@outlook.com>
2020-11-14 19:23:01 +0200

    adding the arabic language voice

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,59 +1,40 @@
-import React, { Component} from 'react'
+import React, { Component} from 'react';
 
 export default class Home extends Component {
     state={
         text:[],
         voices:[],
-        voice_choosen:[]
+        voice_choosen:[],
+        changed_text:[]
     }
-    get_voices(){
-        const synth =window.speechSynthesis;
-        let voices = synth.getVoices()
-        this.setState({voices: voices});
-        }
+
     componentDidMount(){
-        //get voices
-        const synth =window.speechSynthesis;
-        this.get_voices();
-        if(synth.onvoiceschanged !== undefined){
-            synth.onvoiceschanged = this.get_voices()
-        }
+        // console.log(window.responsiveVoice.getVoices());
+        window.responsiveVoice.setDefaultVoice("Arabic Female");
     }
     submithandler(e){
         e.preventDefault();
-        const synth =window.speechSynthesis;
-
-        //Check if speaking
-        if(synth.speaking){
-            console.error("Already speaking...");
-            return;
+        const app = {
+            speak:(word) => {return window.responsiveVoice.speak(word);},
+            more:(word, more) => {return window.responsiveVoice.speak(word, more);},
         }
-        if(this.state.text !== []){
-            const speakText = new SpeechSynthesisUtterance(this.state.text);
-            console.log(speakText);
-            speakText.onend= e=>{
-                console.log("Done speaking...");
-            }
-            
-            speakText.onerror=e=>{
-                console.error('something went wrong')
-            }
-            
-            const selectedVoice = this.state.voice_choosen;
-            console.log(selectedVoice);
-            this.state.voices.forEach(voice =>{
-                if(voice.name === selectedVoice)
-                speakText.voice = voice;
-            })
-            
-            synth.speak(speakText);
-    }
+
+        if(this.state.text === ""){
+            app.speak("Please type something");
+        }
+        else{
+        app.speak(this.state.text); 
+        }
+        app.more(this.state.text, this.state.voice_choosen);
 }
+    changetext(e){
+        e.preventDefault();
+        this.setState({changed_text: this.state.text});
+    }
     render() {
-        // console.log(this.state.voices);
         return (
             <div className="container text-center">
-                <img alt="micro" className="mb-5 w-75 mt-5" src="images/microphone.jpg"></img>
+                <img alt="microphone" className="mb-5 w-50 mt-5" src="images/microphone.jpg"></img>
                 <div className="row">
                     <div className="col-md-6 mx-auto">
                     <form>
@@ -71,16 +52,35 @@ export default class Home extends Component {
                             onChange={event =>this.setState({voice_choosen: event.target.value})} 
                              >
                             <option>Choose</option>
-                            {this.state.voices.map(voice =>
+                            {/* {this.state.voices.map(voice =>
                                 <option>
                                     {voice.name}
                                 </option>
-                                    )}
+                                    )} */}
+                                            <option value='US English Female'>Solovoice (US Female)</option>
+                                            <option value='UK English Male'>Microsoft David (UK Male)</option>
+                                            <option value='Japanese Female'>Oriba (Japanese Female)</option>
+                                            <option value='Arabic Female'>Aesha (Arabic Female)</option>
+                                            <option value='French Male'>Charles (French Male)</option>
+                                            <option value='Swedish Female'>swed (Swedish female)</option>
+                                            <option value='Afrikaans Male'>Tyrone (Afrikaan Male)</option>
+                                            <option value='Hindi Female'>Divya (Hindi Female)</option>
+                                            <option value='Greek Male'>Khis (Greek Male)</option>
+                                            <option value='Spanish Female'>Rodriguez (Spanish Female)</option>
+                                            <option value='Filipino Female'>Fil (Fillipino Male)</option>
+                                            <option value='Russian Male'>vikrel (Russian Male)</option>
+                                            <option value='Chinese Male'>Yin chen (Chinese Male)</option>
+                                            
                              </select>
                         </div>
                         <button className="btn btn-light btn-lg btn-block" onClick={(e)=>this.submithandler(e)}>Submit</button>
+                        <button className="btn btn-light btn-lg btn-block" onClick={(e)=>this.changetext(e)}>Convert</button>
                     </form>
+                        <div className="mb-5 text-of-made-better">
+                                {this.state.changed_text}
+                        </div>
                     </div>
+                       
                 </div>
             </div>
         )
